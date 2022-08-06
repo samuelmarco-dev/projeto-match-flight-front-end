5bdb9126f70cc7b584ab7dba13ab26a07a48bc8e
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-08-06 19:51:57 -0300

    feat: setup create-react-app and building the initial route and rendering component

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import Container from "./style";
+
+function Home() {
+    return (
+        <Container>
+            <h1>Render Home</h1>
+        </Container>
+     );
+}
+
+export default Home;
---
+++ src/components/Home/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.section`
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+`;
+
+export default Container;
