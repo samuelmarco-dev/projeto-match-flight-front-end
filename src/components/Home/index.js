efabbab17ad7c54607d8cb7a0547b381180c5d18
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-08-07 13:08:13 -0300

    feat: splash screen (home) finished

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,69 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IoAirplaneSharp } from 'react-icons/io5';
+import { ThreeDots } from 'react-loader-spinner';
+
+import Paragraph from '../../utils/Paragraph';
+import Button from '../../utils/Button';
 import Container from "./style";
+import "../../assets/css/reset.css";
 
 function Home() {
+    const navigate = useNavigate();
+    const [disable, setDisable] = useState({
+        disableUser: false,
+        disableCompany: false
+    });
+    const [loading, setLoading] = useState({
+        loadingUser: false,
+        loadingCompany: false
+    });
+
+    function avancePage(page) {
+        if(page === 'user'){
+            setDisable({ ...disable, disableUser: true })
+            setLoading({ ...loading, loadingUser: true })
+            setInterval(()=> {
+                navigate(`/${page}`);
+                setDisable({ ...disable, disableUser: false })
+                setLoading({ ...loading, loadingUser: false })
+            }, 500);
+        }
+        if(page === 'company'){
+            setDisable({ ...disable, disableCompany: true })
+            setLoading({ ...loading, loadingCompany: true })
+            setInterval(()=> {  
+                navigate(`/${page}`);
+                setDisable({ ...disable, disableCompany: false })
+                setLoading({ ...loading, loadingCompany: false })
+            }, 500);
+        }
+    }   
+
     return (
         <Container>
-            <h1>Render Home</h1>
+            <header>
+                <IoAirplaneSharp />
+                <Paragraph conteudo="MatchFlight" />
+            </header>
+            <div>
+                {
+                    !loading.loadingCompany ? <Button conteudo="Para empresas" tipo="button" 
+                    disabled={disable.disableCompany} click={()=> avancePage('company')} /> 
+                    :
+                    <Button conteudo={<ThreeDots color="#fff" height={13} />} tipo="button" 
+                    disabled={disable.disableCompany} />
+                }
+                {
+                    !loading.loadingUser ? <Button conteudo="Para usuários" tipo="button" 
+                    disabled={disable.disableUser} click={()=> avancePage('user')} /> 
+                    :
+                    <Button conteudo={<ThreeDots color="#fff" height={13} />} tipo="button" 
+                    disabled={disable.disableUser} />
+                }
+            </div>
         </Container>
-     );
+    );
 }
 
 export default Home;
--- src/components/Home/style.js
+++ src/components/Home/style.js
@@ -4,8 +4,72 @@ const Container = styled.section`
     width: 100%;
     height: 100%;
     position: fixed;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     top: 0;
     left: 0;
+    background-color: #00A7EE;
+
+    header {
+        width: 100%;
+        height: auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 30%;
+        padding: 0 20px;
+
+        svg {
+            color: #FFFFFF;
+            font-size: 120px;
+        }
+        
+        p {
+            width: 100%;
+            height: 70px;
+            font-weight: 800;
+            font-family: 'Bangers', cursive;
+            letter-spacing: 0.08em;
+            font-size: 44px;
+            line-height: 50px;
+            color: #FFFFFF;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-shadow: 4px 3px 6px rgba(0,0,0,0.6);
+        }
+    }
+
+    div {
+        width: 100%;
+        height: auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 0 20px;
+        
+        a {
+            width: 100%;
+            height: auto;
+        }
+        
+        button{
+            width: 100%;
+            height: 48px;
+            background-color: #126BA5;
+            border-radius: 5px;
+            font-weight: 700;
+            font-size: 20px;
+            line-height: 23px;
+            color: #FFFFFF;
+            margin-bottom: 15px;
+            box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.4)
+        }
+    }
 `;
 
 export default Container;
