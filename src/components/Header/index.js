e9e36e3ea19fd084542b258475f58c0a6a625658
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-08-10 00:33:38 -0300

    feat: HeaderScreen component in company and user timeline

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,14 @@
+import { IoMdExit } from "react-icons/io";
+
+import Profile from "../../assets/img/profile.png";
+
 function HeaderScreen() {
     return (
-        <></>
+        <header>
+            <img src={Profile} alt="ImageProfile" />
+            <p>MatchFlight</p>
+            <IoMdExit />
+    </header>
     );
 }
 
